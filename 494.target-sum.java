6476b1beaab32dfe8183e6cd4f10a878f55fb522
Yuchao Zhou <yuchaozh1991@gmail.com>
2019-02-25 22:36:49 -0800

    more

---
+++ 494.target-sum.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=494 lang=java
+ *
+ * [494] Target Sum
+ *
+ * https://leetcode.com/problems/target-sum/description/
+ *
+ * algorithms
+ * Medium (44.79%)
+ * Total Accepted:    83.9K
+ * Total Submissions: 187.3K
+ * Testcase Example:  '[1,1,1,1,1]\n3'
+ *
+ * 
+ * You are given a list of non-negative integers, a1, a2, ..., an, and a
+ * target, S. Now you have 2 symbols + and -. For each integer, you should
+ * choose one from + and - as its new symbol.
+ * ⁠
+ * 
+ * Find out how many ways to assign symbols to make sum of integers equal to
+ * target S.  
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: nums is [1, 1, 1, 1, 1], S is 3. 
+ * Output: 5
+ * Explanation: 
+ * 
+ * -1+1+1+1+1 = 3
+ * +1-1+1+1+1 = 3
+ * +1+1-1+1+1 = 3
+ * +1+1+1-1+1 = 3
+ * +1+1+1+1-1 = 3
+ * 
+ * There are 5 ways to assign symbols to make the sum of nums be target 3.
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The length of the given array is positive and will not exceed 20. 
+ * The sum of elements in the given array will not exceed 1000.
+ * Your output answer is guaranteed to be fitted in a 32-bit integer.
+ * 
+ * 
+ */
+class Solution {
+    public int findTargetSumWays(int[] nums, int s) {
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
+        }
+        return sum < s || (s + sum) % 2 > 0 ? 0 : subsetSum(nums, (s + sum) >>> 1);
+    }
+
+    private int subsetSum(int[] nums, int s) {
+        int[] dp = new int[s + 1];
+        dp[0] = 1;
+        for (int n : nums) {
+            for (int i = s; i >= n; i--) {
+                dp[i] += dp[i - n];
+            }
+        }
+        return dp[s];
+    }
+}
