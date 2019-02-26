faa051691945b5d2888e38c3a72eaf0926033517
Yuchao Zhou <v-yuczh@microsoft.com>
2019-02-25 17:26:09 -0800

    two more

---
+++ 150.evaluate-reverse-polish-notation.java
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode id=150 lang=java
+ *
+ * [150] Evaluate Reverse Polish Notation
+ *
+ * https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+ *
+ * algorithms
+ * Medium (31.29%)
+ * Total Accepted:    149.4K
+ * Total Submissions: 477.4K
+ * Testcase Example:  '["2","1","+","3","*"]'
+ *
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ * 
+ * Valid operators are +, -, *, /. Each operand may be an integer or another
+ * expression.
+ * 
+ * Note:
+ * 
+ * 
+ * Division between two integers should truncate toward zero.
+ * The given RPN expression is always valid. That means the expression would
+ * always evaluate to a result and there won't be any divide by zero
+ * operation.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: ["2", "1", "+", "3", "*"]
+ * Output: 9
+ * Explanation: ((2 + 1) * 3) = 9
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: ["4", "13", "5", "/", "+"]
+ * Output: 6
+ * Explanation: (4 + (13 / 5)) = 6
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+ * Output: 22
+ * Explanation: 
+ * ⁠ ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+ * = ((10 * (6 / (12 * -11))) + 17) + 5
+ * = ((10 * (6 / -132)) + 17) + 5
+ * = ((10 * 0) + 17) + 5
+ * = (0 + 17) + 5
+ * = 17 + 5
+ * = 22
+ * 
+ * 
+ */
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new LinkedList<>();
+        int first;
+        int second;
+        for(String s : tokens) {
+            switch (s) {
+                case "+":
+                    stack.offerLast(stack.pollLast() + stack.pollLast()); 
+                    break;
+                case "-":
+                    first = stack.pollLast();
+                    second = stack.pollLast();
+                    stack.offerLast(second - first);
+                    break;
+                case "*":
+                    stack.offerLast(stack.pollLast() * stack.pollLast());
+                    break;
+                case "/":
+                    first = stack.pollLast();
+                    second = stack.pollLast();
+                    stack.offerLast(second / first);
+                    break;
+                default:
+                    stack.offerLast(Integer.parseInt(s));
+                    break;
+            }
+        }
+        return stack.pollLast();
+    }
+}
