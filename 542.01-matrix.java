e170d73a692660162afba0cb2d1b022b05874d7a
Zhou Yuchao <yuchaozh1991@gmail.com>
2019-02-27 22:37:26 -0800

    1

---
+++ 542.01-matrix.java
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode id=542 lang=java
+ *
+ * [542] 01 Matrix
+ *
+ * https://leetcode.com/problems/01-matrix/description/
+ *
+ * algorithms
+ * Medium (34.66%)
+ * Total Accepted:    36.7K
+ * Total Submissions: 105.9K
+ * Testcase Example:  '[[0,0,0],[0,1,0],[0,0,0]]'
+ *
+ * 
+ * Given a matrix consists of 0 and 1, find the distance of the nearest 0 for
+ * each cell.
+ * 
+ * The distance between two adjacent cells is 1.
+ * 
+ * Example 1: 
+ * Input:
+ * 
+ * 0 0 0
+ * 0 1 0
+ * 0 0 0
+ * 
+ * Output:
+ * 
+ * 0 0 0
+ * 0 1 0
+ * 0 0 0
+ * 
+ * 
+ * 
+ * Example 2: 
+ * Input:
+ * 
+ * 0 0 0
+ * 0 1 0
+ * 1 1 1
+ * 
+ * Output:
+ * 
+ * 0 0 0
+ * 0 1 0
+ * 1 2 1
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The number of elements of the given matrix will not exceed 10,000.
+ * There are at least one 0 in the given matrix.
+ * The cells are adjacent in only four directions: up, down, left and right.
+ * 
+ * 
+ * 
+ */
+class Solution {
+    public int[][] updateMatrix(int[][] matrix) {
+        if (matrix == null || matrix[0] == null) {
+            return matrix;
+        }
+        int row = matrix.length;
+        int col = matrix[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 0) {
+                    queue.offer(new int[] {i, j});
+                } else {
+                    matrix[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        int[][] dirs = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            for (int[] d : dirs) {
+                int r = cell[0] + d[0];
+                int c = cell[1] + d[1];
+                if (r < 0 || c < 0 || r >= row || c >= col || matrix[r][c] <= matrix[cell[0]][cell[1]] + 1) {
+                    continue;
+                }
+                queue.add(new int[] {r, c});
+                matrix[r][c] = matrix[cell[0]][cell[1]] + 1;
+            }
+        }
+
+        return matrix;
+    }
+}
+
