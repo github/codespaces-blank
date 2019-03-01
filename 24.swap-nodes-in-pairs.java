2f63280f2ca85ad9198f002cbc04d989906e6d4f
Yuchao Zhou <yuchaozh1991@gmail.com>
2019-03-01 00:21:00 -0800

    finish

--- 24.swap-nodes-in-pairs.java
+++ 24.swap-nodes-in-pairs.java
@@ -35,7 +35,13 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
     }
 }
 
