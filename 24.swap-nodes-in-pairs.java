3a681785e011b59db33568680df7c38974431b73
Yuchao Zhou <v-yuczh@microsoft.com>
2019-03-01 14:13:21 -0800

    add more solution

--- 24.swap-nodes-in-pairs.java
+++ 24.swap-nodes-in-pairs.java
@@ -33,15 +33,72 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+// class Solution {
+//     public ListNode swapPairs(ListNode head) {
+//         if (head == null || head.next == null) {
+//             return head;
+//         }
+//         ListNode n = head.next;
+//         head.next = swapPairs(head.next.next);
+//         n.next = head;
+//         return n;
+//     }
+// }
+
+// class Solution {
+//     public ListNode swapPairs(ListNode head) {
+//         if (head == null || head.next == null) {
+//             return head;
+//         }
+//         ListNode second = head.next;
+//         ListNode third = second.next;
+        
+//         second.next = head;
+//         head.next = swapPairs(third);
+
+//         return second;
+//     }
+// }
+
+// class Solution {
+//     public ListNode swapPairs(ListNode head) {
+//         ListNode dummy = new ListNode(0);
+//         dummy.next = head;
+//         ListNode current = dummy;
+//         while (current.next != null && current.next.next != null) {
+//             ListNode first = current.next;
+//             ListNode second = current.next.next;
+
+//             first.next = second.next;
+//             current.next = second;
+//             second.next = first;
+            
+//             current = first;
+//         }
+//         return dummy.next;
+//     }
+// }
+
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+
+            // this one must be first. set first.next = null.
+            // if you change second.next first, then You cannot set first.next to correct element
+            first.next = second.next;
+            // but for the following two, no matter what order they are.
+            // you can change the order
+            second.next = first;
+            current.next = second;
+            
+            current = first;
         }
-        ListNode n = head.next;
-        head.next = swapPairs(head.next.next);
-        n.next = head;
-        return n;
+        return dummy.next;
     }
 }
 
