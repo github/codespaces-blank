8009d2b99c03da3e853022ebb0fd8a5d8e76ed6e
Zhou Yuchao <yuchaozh1991@gmail.com>
2019-03-04 21:37:40 -0800

    Create 70.climbing-stairs.java

---
+++ 70.climbing-stairs.java
@@ -0,0 +1,97 @@
+/*
+ * @lc app=leetcode id=70 lang=java
+ *
+ * [70] Climbing Stairs
+ *
+ * https://leetcode.com/problems/climbing-stairs/description/
+ *
+ * algorithms
+ * Easy (43.40%)
+ * Total Accepted:    358.9K
+ * Total Submissions: 825.6K
+ * Testcase Example:  '2'
+ *
+ * You are climbing a stair case. It takes n steps to reach to the top.
+ * 
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can
+ * you climb to the top?
+ * 
+ * Note: Given n will be a positive integer.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ * 
+ * 
+ */
+
+// same as fibonacci number
+// recursive
+// class Solution {
+//     public int climbStairs(int n) {
+//         if (n == 0) {
+//             return 0;
+//         } else if (n == 1) {
+//             return 1;
+//         } else if (n == 2) {
+//             return 2;
+//         }
+//         return climbStairs(n - 1) + climbStairs(n - 2);
+//     }
+// }
+
+// DP
+// class Solution {
+//     public int climbStairs(int n) {
+//         if (n == 0) {
+//             return 0;
+//         }
+//         if (n == 1) {
+//             return 1;
+//         }
+//         int first = 1;
+//         int second = 2;
+//         for (int i = 3; i <= n; i++) {
+//             int third = first + second;
+//             first = second;
+//             second = third;
+//         }
+//         return second;
+//     }
+// }
+
+// DP 2
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <=n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
+
