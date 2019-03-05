eae3388b9140fd59d6a89d4f2da35b3f4f17318f
Yuchao Zhou <yuchaozh1991@gmail.com>
2019-03-04 17:19:11 -0800

    Update 206.reverse-linked-list.java

--- 206.reverse-linked-list.java
+++ 206.reverse-linked-list.java
@@ -54,13 +54,36 @@
 //     }
 // }
 
+// https://leetcode.com/explore/learn/card/recursion-i/251/scenario-i-recurrence-relation/2379/
+/**
+ * The recursive version is slightly trickier and the key is to work backwards. Assume that the rest of the list had already been reversed, now how do I reverse the front part? Let's assume the list is: n1 → … → nk-1 → nk → nk+1 → … → nm → Ø
+
+Assume from node nk+1 to nm had been reversed and you are at node nk.
+
+n1 → … → nk-1 → nk → nk+1 ← … ← nm
+
+We want nk+1’s next node to point to nk.
+
+So,
+
+nk.next.next = nk;
+
+Be very careful that n1's next must point to Ø. If you forget about this, your linked list has a cycle in it. This bug could be caught if you test your code with a linked list of size 2.
+
+当前的node就是head 
+Recursion Function就是一个break point. 
+在Recursion Function上面的操作是进入下一层之前要执行的操作
+在Recursion Function下面的操作是返回上一层后要执行的操作
+*/
 /* Recrusive */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        // 进入下一层之前要执行的操作
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode newHead = reverseList(head.next);
+        ListNode newHead = reverseList(head.next);  // break point
+        // 返回上一层之后要执行的操作
         head.next.next = head;
         head.next = null;
         return newHead;
