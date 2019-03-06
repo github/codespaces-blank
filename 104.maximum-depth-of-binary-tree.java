0f7596d4dd84509d2c402f76aa2c1b13d2e23c55
Zhou Yuchao <yuchaozh1991@gmail.com>
2019-03-05 19:14:13 -0800

    Create 104.maximum-depth-of-binary-tree.java

---
+++ 104.maximum-depth-of-binary-tree.java
@@ -0,0 +1,91 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode id=104 lang=java
+ *
+ * [104] Maximum Depth of Binary Tree
+ *
+ * https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (59.16%)
+ * Total Accepted:    457.7K
+ * Total Submissions: 772.8K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, find its maximum depth.
+ * 
+ * The maximum depth is the number of nodes along the longest path from the
+ * root node down to the farthest leaf node.
+ * 
+ * Note: A leaf is a node with no children.
+ * 
+ * Example:
+ * 
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * return its depth = 3.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+ // Recursive
+class Solution {
+    public int maxDepth(TreeNode root) {
+        // base case
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        
+        return Math.max(left, right) + 1;
+    }
+}
+
+// Iterative - BFS
+// class Solution {
+//     public int maxDepth(TreeNode root) {
+//         if(root == null) {
+//             return 0;
+//         }
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.offer(root);
+//         int count = 0;
+//         while(!queue.isEmpty()) {
+//             int size = queue.size();
+//             while(size > 0) {
+//                 TreeNode node = queue.poll();
+//                 if(node.left != null) {
+//                     queue.offer(node.left);
+//                 }
+//                 if(node.right != null) {
+//                     queue.offer(node.right);
+//                 }
+//                 size--;
+//             }
+//             count++;
+//         }
+//         return count;
+//     }
+// }
+
