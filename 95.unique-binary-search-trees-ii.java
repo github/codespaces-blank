7a7d5b151ad4d5f81da68f5afd877f07cb47c59f
Zhou Yuchao <yuchaozh1991@gmail.com>
2019-03-05 22:21:46 -0800

    Create 95.unique-binary-search-trees-ii.java

---
+++ 95.unique-binary-search-trees-ii.java
@@ -0,0 +1,88 @@
+import java.util.LinkedList;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode id=95 lang=java
+ *
+ * [95] Unique Binary Search Trees II
+ *
+ * https://leetcode.com/problems/unique-binary-search-trees-ii/description/
+ *
+ * algorithms
+ * Medium (34.77%)
+ * Total Accepted:    128.8K
+ * Total Submissions: 369.9K
+ * Testcase Example:  '3'
+ *
+ * Given an integer n, generate all structurally unique BST's (binary search
+ * trees) that store values 1 ... n.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 3
+ * Output:
+ * [
+ * [1,null,3,2],
+ * [3,2,null,1],
+ * [3,1,null,null,2],
+ * [2,1,3],
+ * [1,null,2,null,3]
+ * ]
+ * Explanation:
+ * The above output corresponds to the 5 unique BST's shown below:
+ * 
+ * ⁠  1         3     3      2      1
+ * ⁠   \       /     /      / \      \
+ * ⁠    3     2     1      1   3      2
+ * ⁠   /     /       \                 \
+ * ⁠  2     1         2                 3
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generate_tree(1, n);
+    }
+
+    private LinkedList<TreeNode> generate_tree(int start, int end) {
+        LinkedList<TreeNode> all_trees = new LinkedList<>();
+        if (start > end) {
+            all_trees.add(null);
+            return all_trees;
+        }
+        // pick up a root
+        for (int i = start; i <= end; i++) {
+            // all posisible left subtrees if i is choosen to be a root
+            LinkedList<TreeNode> left_trees = generate_tree(start, i - 1);
+
+            // all possible right subtrees if i is choosen to be a root
+            LinkedList<TreeNode> right_trees = generate_tree(i + 1, end);
+
+            // connect left and right trees to the root i
+            for (TreeNode l : left_trees) {
+                for (TreeNode r : right_trees) {
+                    TreeNode current_tree = new TreeNode(i);
+                    current_tree.left = l;
+                    current_tree.right = r;
+                    all_trees.add(current_tree);
+                }
+            }
+        }
+        return all_trees;
+    }
+}
+
