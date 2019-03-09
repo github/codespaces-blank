8a9b54e882f52c114cbd8da820975c498a1521b1
Yuchao Zhou <yuchaozh1991@gmail.com>
2019-03-08 21:50:13 -0800

    Create 8.string-to-integer-atoi.java

---
+++ 8.string-to-integer-atoi.java
@@ -0,0 +1,116 @@
+/*
+ * @lc app=leetcode id=8 lang=java
+ *
+ * [8] String to Integer (atoi)
+ *
+ * https://leetcode.com/problems/string-to-integer-atoi/description/
+ *
+ * algorithms
+ * Medium (14.47%)
+ * Total Accepted:    331.4K
+ * Total Submissions: 2.3M
+ * Testcase Example:  '"42"'
+ *
+ * Implement atoi which converts a string to an integer.
+ * 
+ * The function first discards as many whitespace characters as necessary until
+ * the first non-whitespace character is found. Then, starting from this
+ * character, takes an optional initial plus or minus sign followed by as many
+ * numerical digits as possible, and interprets them as a numerical value.
+ * 
+ * The string can contain additional characters after those that form the
+ * integral number, which are ignored and have no effect on the behavior of
+ * this function.
+ * 
+ * If the first sequence of non-whitespace characters in str is not a valid
+ * integral number, or if no such sequence exists because either str is empty
+ * or it contains only whitespace characters, no conversion is performed.
+ * 
+ * If no valid conversion could be performed, a zero value is returned.
+ * 
+ * Note:
+ * 
+ * 
+ * Only the space character ' ' is considered as whitespace character.
+ * Assume we are dealing with an environment which could only store integers
+ * within the 32-bit signed integer range: [−2^31,  2^31 − 1]. If the numerical
+ * value is out of the range of representable values, INT_MAX (2^31 − 1) or
+ * INT_MIN (−2^31) is returned.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "42"
+ * Output: 42
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "   -42"
+ * Output: -42
+ * Explanation: The first non-whitespace character is '-', which is the minus
+ * sign.
+ * Then take as many numerical digits as possible, which gets 42.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "4193 with words"
+ * Output: 4193
+ * Explanation: Conversion stops at digit '3' as the next character is not a
+ * numerical digit.
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: "words and 987"
+ * Output: 0
+ * Explanation: The first non-whitespace character is 'w', which is not a
+ * numerical 
+ * digit or a +/- sign. Therefore no valid conversion could be performed.
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: "-91283472332"
+ * Output: -2147483648
+ * Explanation: The number "-91283472332" is out of the range of a 32-bit
+ * signed integer.
+ * Thefore INT_MIN (−2^31) is returned.
+ * 
+ */
+class Solution {
+    private final int maxDiv10 = Integer.MAX_VALUE / 10;
+
+    public int myAtoi(String str) {
+        int i = 0;
+        int n = str.length();
+        while (i < n && Character.isWhitespace(str.charAt(i))) {
+            i++;
+        }
+        
+        int sign = 1;
+        if (i < n && str.charAt(i) == '+') {
+            i++;
+        } else if (i < n && str.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        }
+
+        int num = 0;
+        while (i < n && Character.isDigit(str.charAt(i))) {
+            int digit = Character.getNumericValue(str.charAt(i));
+            if (num > maxDiv10 || num == maxDiv10 && digit >= 8) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            num = num * 10 + digit;
+            i++;
+        }
+        return sign * num;
+    }
+}
+
