b5f856bfefd84feacee7822745e4e6d237deac40
Yuchao Zhou <yuchaozh1991@gmail.com>
2019-03-10 00:35:15 -0800

    Create 7.reverse-integer.java

---
+++ 7.reverse-integer.java
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=7 lang=java
+ *
+ * [7] Reverse Integer
+ *
+ * https://leetcode.com/problems/reverse-integer/description/
+ *
+ * algorithms
+ * Easy (25.10%)
+ * Total Accepted:    622.3K
+ * Total Submissions: 2.5M
+ * Testcase Example:  '123'
+ *
+ * Given a 32-bit signed integer, reverse digits of an integer.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 123
+ * Output: 321
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: -123
+ * Output: -321
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 120
+ * Output: 21
+ * 
+ * 
+ * Note:
+ * Assume we are dealing with an environment which could only store integers
+ * within the 32-bit signed integer range: [−2^31,  2^31 − 1]. For the purpose
+ * of this problem, assume that your function returns 0 when the reversed
+ * integer overflows.
+ * 
+ */
+
+/**
+We want to repeatedly "pop" the last digit off of xx and "push" it to the back of the \text{rev}rev. In the end, \text{rev}rev will be the reverse of the xx.
+
+To "pop" and "push" digits without the help of some auxiliary stack/array, we can use math.
+
+//pop operation:
+pop = x % 10;
+x /= 10;
+
+//push operation:
+temp = rev * 10 + pop;
+rev = temp;
+
+However, this approach is dangerous, because the statement temp = rev * 10 + pop can cause overflow.
+
+Luckily, it is easy to check beforehand whether or this statement would cause an overflow.
+ */
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}
+
