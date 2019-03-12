228539b2e4a3f6f13699c6a6126b5301109dcdf3
Yuchao Zhou <yuchaozh1991@gmail.com>
2019-03-11 20:57:52 -0700

    Update 2.add-two-numbers.java

--- 2.add-two-numbers.java
+++ 2.add-two-numbers.java
@@ -69,3 +69,15 @@ class Solution {
     }
 }
 
+// recursive
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        int val = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
+        ListNode head = new ListNode(val % 10);
+        head.next = addTwoNumbers(l1 == null ? null : l1.next, l2 == null ? null : l2.next);
+        if (val >= 10) head.next = addTwoNumbers(head.next, new ListNode(1));
+        return head;
+    }
+}
+
