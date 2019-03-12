30cc3581d6c5fef385a401c0afb095ad562b9c63
Yuchao Zhou <yuchaozh1991@gmail.com>
2019-03-11 20:40:27 -0700

    Update 21.merge-two-sorted-lists.java

--- 21.merge-two-sorted-lists.java
+++ 21.merge-two-sorted-lists.java
@@ -36,23 +36,24 @@ import javax.management.ListenerNotFoundException;
 // we determine which of l1 and l2 has a smaller head, and recursively set the next value for that head to the next merge result
 
 // recursive
-// class Solution {
-//     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-//         if (l1 == null) {
-//             return l2;
-//         }
-//         if (l2 == null) {
-//             return l1;
-//         }
-//         if (l1.val <= l2.val) {
-//             l1.next = mergeTwoLists(l1.next, l2);
-//             return l1;
-//         } else {
-//             l2.next = mergeTwoLists(l1, l2.next);
-//             return l2;
-//         }
-//     }
-// }
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val <= l2.val) {
+            // 从后到前
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
 
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
