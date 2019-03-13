43cde5da741935df7f3eb2256e78122ebb7ca8df
Yuchao Zhou <yuchaozh1991@gmail.com>
2019-03-12 19:45:03 -0700

    Update 98.validate-binary-search-tree.java

--- 98.validate-binary-search-tree.java
+++ 98.validate-binary-search-tree.java
@@ -59,7 +59,16 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        
+        return valid(root, null, null);
+    }
+
+    private boolean valid(TreeNode root, Integer low, Integer high) {
+        if (root == null) {
+            return true;
+        }
+        return (low == null || root.val > low) && (high == null || root.val < high) 
+        && valid(root.left, low, root.val) 
+        && valid(root.right, root.val, high);
     }
 }
 
