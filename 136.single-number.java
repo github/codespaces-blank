c34c4ca5b7e02dfd3869af93f6221191fdfd5325
Yuchao Zhou <yuchaozh1991@gmail.com>
2019-03-14 22:09:26 -0700

    Create 136.single-number.java

---
+++ 136.single-number.java
@@ -0,0 +1,87 @@
+
+/*
+ * @lc app=leetcode id=136 lang=java
+ *
+ * [136] Single Number
+ *
+ * https://leetcode.com/problems/single-number/description/
+ *
+ * algorithms
+ * Easy (58.86%)
+ * Total Accepted:    424.9K
+ * Total Submissions: 718K
+ * Testcase Example:  '[2,2,1]'
+ *
+ * Given a non-empty array of integers, every element appears twice except for
+ * one. Find that single one.
+ * 
+ * Note:
+ * 
+ * Your algorithm should have a linear runtime complexity. Could you implement
+ * it without using extra memory?
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [2,2,1]
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ * 
+ * 
+ */
+
+ // hashmap, two loop
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : nums) {
+            if (map.get(num) == 1) {
+                return num;
+            }
+        }
+        throw new IllegalArgumentException("no single element"); 
+    }
+}
+
+// Hashset, one loop
+// check current value is in the set already? Yes, means this is second time, remove 
+// the first one from set. 
+// Else, this is first time to have the number, put it into set
+// after one loop, the only element in the set only appears once
+class Solution {
+    public int singleNumber(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                set.remove(num);
+            } else {
+                set.add(num);
+            }
+        }
+        return set.iterator().next();
+    }
+}
+
+// Bit Manipulation
+// XOR: same -> 0; not same -> 1
+// a ⊕ b ⊕ a = (a ⊕ a) ⊕ b = 0 ⊕ b = b
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
+
