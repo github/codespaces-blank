0764f5ae3ada8fb48620c2eaee7c89b23da39fab
Yuchao Zhou <yuchaozh1991@gmail.com>
2019-03-16 23:57:38 -0700

    Update 13.roman-to-integer.java

--- 13.roman-to-integer.java
+++ 13.roman-to-integer.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 /*
  * @lc app=leetcode id=13 lang=java
  *
@@ -79,7 +81,83 @@
  */
 class Solution {
     public int romanToInt(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int s1 = romanToNumber(s.charAt(i));
+            if (i + 1 < s.length()) {
+                int s2 = romanToNumber(s.charAt(i + 1));
+                if (s1 >= s2) {
+                    // current integer is bigger than next, ignore
+                    result = result + s1;
+                } else {
+                    // current integer is smaller than next, it means we need to subtract current from next
+                    // remember to i++, because next element is used at current round
+                    result = result + s2 - s1;
+                    i++;
+                }
+            } else { // this is last char in input string
+                result = result + s1;
+                i++;
+            }
+        }
+        return result;
+    }
+
+    private int romanToNumber(char r) {
+        int ans = 0;
+        switch (r) {
+            case 'I':
+                ans = 1;
+                break;
+            case 'V':
+                ans = 5;
+                break;
+            case 'X': 
+                ans = 10;
+                break;
+            case 'L':
+                ans = 50;
+                break;
+            case 'C':
+                ans = 100;
+                break;
+            case 'D':
+                ans = 500;
+                break;
+            case 'M':
+                ans = 1000;
+                break;
+            default:
+                ans = -1;
+        }
+        return ans;
+    }
+}
+
+// When the prev is smaller than current integer, we need to subtract the prev one.
+// However, in this solution, we already added the prev one. So we need to subtract twice of prev one
+// MXC  1010 + (100 - 2 * 10)
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>(); {
+            put('I', 1);
+            put('V', 5);
+            put('X', 10);
+            put('L', 50);
+            put('C', 100);
+            put('D', 500);
+            put('M', 1000);
+        }
+        int prev = 0;
+        int total = 0;
         
+        for (char c : s.toCharArray()) {
+            int current = map.get(c);
+            total += (current > prev) ? (current - 2 * prev) : current;
+            prev = current;
+        }
+
+        return total;
     }
 }
 
