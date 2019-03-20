a9f8ee522ab7c640f9861e0bb4878136d88e4143
Zhou Yuchao <yuchaozh1991@gmail.com>
2019-03-19 20:50:47 -0700

    Create 53.maximum-subarray.java

---
+++ 53.maximum-subarray.java
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=53 lang=java
+ *
+ * [53] Maximum Subarray
+ *
+ * https://leetcode.com/problems/maximum-subarray/description/
+ *
+ * algorithms
+ * Easy (42.77%)
+ * Total Accepted:    478.6K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[-2,1,-3,4,-1,2,1,-5,4]'
+ *
+ * Given an integer array nums, find the contiguous subarray (containing at
+ * least one number) which has the largest sum and return its sum.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [-2,1,-3,4,-1,2,1,-5,4],
+ * Output: 6
+ * Explanation: [4,-1,2,1] has the largest sum = 6.
+ * 
+ * 
+ * Follow up:
+ * 
+ * If you have figured out the O(n) solution, try coding another solution using
+ * the divide and conquer approach, which is more subtle.
+ * 
+ */
+// f(k) = Maximum sum of subarray ending at index k
+// f(k) = max(f(k - 1) + A[k], A[k])
+// maxEndingHere is the maximum sum of subarray that must end at index k, while
+// maxSoFar is the global maximum subarray sum
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxEndingHere = nums[0];
+        int maxSoFar = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
+            maxSoFar = Math.max(maxEndingHere, maxSoFar);
+        }
+        return maxSoFar;
+    }
+}
+
