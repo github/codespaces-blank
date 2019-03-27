6f70cd58aa36687507a147c5c1fc92a61b715809
Yuchao Zhou <yuchaozh1991@gmail.com>
2019-03-27 15:21:52 -0700

    Create 75.sort-colors.java

---
+++ 75.sort-colors.java
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=75 lang=java
+ *
+ * [75] Sort Colors
+ *
+ * https://leetcode.com/problems/sort-colors/description/
+ *
+ * algorithms
+ * Medium (41.27%)
+ * Total Accepted:    299.7K
+ * Total Submissions: 721.1K
+ * Testcase Example:  '[2,0,2,1,1,0]'
+ *
+ * Given an array with n objects colored red, white or blue, sort them in-place
+ * so that objects of the same color are adjacent, with the colors in the order
+ * red, white and blue.
+ * 
+ * Here, we will use the integers 0, 1, and 2 to represent the color red,
+ * white, and blue respectively.
+ * 
+ * Note: You are not suppose to use the library's sort function for this
+ * problem.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [2,0,2,1,1,0]
+ * Output: [0,0,1,1,2,2]
+ * 
+ * Follow up:
+ * 
+ * 
+ * A rather straight forward solution is a two-pass algorithm using counting
+ * sort.
+ * First, iterate the array counting number of 0's, 1's, and 2's, then
+ * overwrite array with total number of 0's, then 1's and followed by 2's.
+ * Could you come up with a one-pass algorithm using only constant space?
+ * 
+ * 
+ */
+
+ // two pass
+class Solution {
+    public void sortColors(int[] nums) {
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) count0++;
+            if (nums[i] == 1) count1++;
+            if (nums[i] == 2) count2++;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (i < count0) {
+                nums[i] = 0;
+            } else if (i < count0 + count1) {
+                nums[i] = 1;
+            } else {
+                nums[i] = 2;
+            }
+        }
+    }
+}
+
+// One Pass
+class Solution {
+    public void sortColors(int[] nums) {
+        int p1 = 0;
+        int p2 = nums.length - 1;
+        int index = 0;
+        while (index <= p2) {
+            if (nums[index] == 0) {
+                nums[index] = nums[p1];
+                nums[p1] = 0;
+                p1++;
+            }
+            if (nums[index] == 2) {
+                nums[index] = nums[p2];
+                nums[p2] = 2;
+                p2--;
+                index--;
+            }
+            index++;
+        }
+    }
+}
