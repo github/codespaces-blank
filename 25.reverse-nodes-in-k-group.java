575303e681095bf561662392321be7d3ce4e87bc
Yuchao Zhou <yuchaozh1991@gmail.com>
2019-03-27 17:41:04 -0700

    Create 25.reverse-nodes-in-k-group.java

---
+++ 25.reverse-nodes-in-k-group.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=25 lang=java
+ *
+ * [25] Reverse Nodes in k-Group
+ *
+ * https://leetcode.com/problems/reverse-nodes-in-k-group/description/
+ *
+ * algorithms
+ * Hard (35.25%)
+ * Total Accepted:    173.2K
+ * Total Submissions: 485.5K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * Given a linked list, reverse the nodes of a linked list k at a time and
+ * return its modified list.
+ * 
+ * k is a positive integer and is less than or equal to the length of the
+ * linked list. If the number of nodes is not a multiple of k then left-out
+ * nodes in the end should remain as it is.
+ * 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * Given this linked list: 1->2->3->4->5
+ * 
+ * For k = 2, you should return: 2->1->4->3->5
+ * 
+ * For k = 3, you should return: 3->2->1->4->5
+ * 
+ * Note:
+ * 
+ * 
+ * Only constant extra memory is allowed.
+ * You may not alter the values in the list's nodes, only nodes itself may be
+ * changed.
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        
+    }
+}
+
