460ee88a4d2f2ea659318478f144040c6cce2849
Yuchao Zhou <yuczho@microsoft.com>
2019-07-13 15:20:34 -0700

    add

---
+++ 985.sum-of-even-numbers-after-queries.java
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode id=985 lang=java
+ *
+ * [985] Sum of Even Numbers After Queries
+ *
+ * https://leetcode.com/problems/sum-of-even-numbers-after-queries/description/
+ *
+ * algorithms
+ * Easy (63.42%)
+ * Likes:    163
+ * Dislikes: 75
+ * Total Accepted:    23.3K
+ * Total Submissions: 36.8K
+ * Testcase Example:  '[1,2,3,4]\n[[1,0],[-3,1],[-4,0],[2,3]]'
+ *
+ * We have an array A of integers, and an array queries of queries.
+ * 
+ * For the i-th query val = queries[i][0], index = queries[i][1], we add val to
+ * A[index].  Then, the answer to the i-th query is the sum of the even values
+ * of A.
+ * 
+ * (Here, the given index = queries[i][1] is a 0-based index, and each query
+ * permanently modifies the array A.)
+ * 
+ * Return the answer to all queries.  Your answer array should have answer[i]
+ * as the answer to the i-th query.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: A = [1,2,3,4], queries = [[1,0],[-3,1],[-4,0],[2,3]]
+ * Output: [8,6,2,4]
+ * Explanation: 
+ * At the beginning, the array is [1,2,3,4].
+ * After adding 1 to A[0], the array is [2,2,3,4], and the sum of even values
+ * is 2 + 2 + 4 = 8.
+ * After adding -3 to A[1], the array is [2,-1,3,4], and the sum of even values
+ * is 2 + 4 = 6.
+ * After adding -4 to A[0], the array is [-2,-1,3,4], and the sum of even
+ * values is -2 + 4 = 2.
+ * After adding 2 to A[3], the array is [-2,-1,3,6], and the sum of even values
+ * is -2 + 6 = 4.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 1 <= A.length <= 10000
+ * -10000 <= A[i] <= 10000
+ * 1 <= queries.length <= 10000
+ * -10000 <= queries[i][0] <= 10000
+ * 0 <= queries[i][1] < A.length
+ * 
+ * 
+ */
+class Solution {
+    public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
+        // get sum for all even numbers 
+        int sum = 0; 
+        for (int x : A) {
+            if (x % 2 == 0) {
+                sum += x;
+            }
+        }
+        
+        int[] result = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int val = queries[i][0];
+            int index = queries[i][1];
+            
+            // if original number is even, we remove it from sum
+            // else if it is odd, we dont remove it since we dont even added it
+            if (A[index] % 2 == 0) {
+                sum -= A[index];
+            }
+            A[index] += val;
+            
+            // if new number is even, we add it back to sum
+            // else if it is odd, we dont add it back
+            if (A[index] % 2 == 0) {
+                sum += A[index];
+            }
+            result[i] = sum;
+        }
+        
+        return result;
+    }
+}
+
