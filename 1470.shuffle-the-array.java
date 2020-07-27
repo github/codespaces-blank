ba17490032ea204ff9efa238fadf36aa44094ba8
Yuchao Zhou <yuchaozh1991@gmail.com>
2020-07-26 23:25:13 -0700

    add two problems

---
+++ 1470.shuffle-the-array.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=1470 lang=java
+ *
+ * [1470] Shuffle the Array
+ */
+
+// @lc code=start
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        if (nums == null || nums.length == 0) {
+            return new int[] {};
+        }
+        
+        int[] result = new int[nums.length];
+        for (int i = 0; i < n; i++) {
+            result[2 * i] = nums[i];
+            result[2 * i + 1] = nums[n + i];
+        }
+        
+        return result;
+    }
+}
+// @lc code=end
+
---
+++ 1480.running-sum-of-1-d-array.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=1480 lang=java
+ *
+ * [1480] Running Sum of 1d Array
+ */
+
+// @lc code=start
+// class Solution {
+//     public int[] runningSum(int[] nums) {
+//         if (nums == null || nums.length == 0) {
+//             return new int[] {};
+//         }
+        
+//         int[] result = new int[nums.length];
+//         int sum = 0;
+//         for (int i = 0; i < nums.length; i++) {
+//             sum += nums[i];
+//             result[i] = sum;
+//         }
+        
+//         return result;
+//     }
+// }
+
+// better solution
+class Solution {
+    public int[] runningSum(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new int[] {};
+        }
+        
+        int index = 1;
+        while (index < nums.length) {
+            nums[index] += nums[index - 1];
+            index++;
+        }
+
+        return nums;
+    }
+}
+// @lc code=end
+
