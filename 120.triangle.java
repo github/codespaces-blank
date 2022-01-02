cff7d19a12436c74e9eebf1308d560942cb5b159
Zhou Yuchao <yuchaozhou2014@u.northwestern.edu>
2022-01-01 19:37:06 -0800

    add
    add

---
+++ 120.triangle.java
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=120 lang=java
+ *
+ * [120] Triangle
+ *
+ * https://leetcode.com/problems/triangle/description/
+ *
+ * algorithms
+ * Medium (43.99%)
+ * Likes:    2784
+ * Dislikes: 310
+ * Total Accepted:    294.3K
+ * Total Submissions: 638K
+ * Testcase Example:  '[[2],[3,4],[6,5,7],[4,1,8,3]]'
+ *
+ * Given a triangle array, return the minimum path sum from top to bottom.
+ * 
+ * For each step, you may move to an adjacent number of the row below. More
+ * formally, if you are on index i on the current row, you may move to either
+ * index i or index i + 1 on the next row.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+ * Output: 11
+ * Explanation: The triangle looks like:
+ * ⁠  2
+ * ⁠ 3 4
+ * ⁠6 5 7
+ * 4 1 8 3
+ * The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined
+ * above).
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: triangle = [[-10]]
+ * Output: -10
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= triangle.length <= 200
+ * triangle[0].length == 1
+ * triangle[i].length == triangle[i - 1].length + 1
+ * -10^4 <= triangle[i][j] <= 10^4
+ * 
+ * 
+ * 
+ * Follow up: Could you do this using only O(n) extra space, where n is the
+ * total number of rows in the triangle?
+ */
+
+// @lc code=start
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        
+    }
+}
+// @lc code=end
+
---
+++ 18.4-sum.java
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=18 lang=java
+ *
+ * [18] 4Sum
+ *
+ * https://leetcode.com/problems/4sum/description/
+ *
+ * algorithms
+ * Medium (33.57%)
+ * Likes:    3198
+ * Dislikes: 406
+ * Total Accepted:    407.2K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '[1,0,-1,0,-2,2]\n0'
+ *
+ * Given an array nums of n integers, return an array of all the unique
+ * quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+ * 
+ * 
+ * 0 <= a, b, c, d < n
+ * a, b, c, and d are distinct.
+ * nums[a] + nums[b] + nums[c] + nums[d] == target
+ * 
+ * 
+ * You may return the answer in any order.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,0,-1,0,-2,2], target = 0
+ * Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [2,2,2,2,2], target = 8
+ * Output: [[2,2,2,2]]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 200
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        
+    }
+}
+// @lc code=end
+
