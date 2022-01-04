c8cf269ea3166d092836de09664384a7a761e479
Zhou Yuchao <yuchaozhou2014@u.northwestern.edu>
2022-01-04 11:18:32 -0800

    add

---
+++ 121.best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=121 lang=java
+ *
+ * [121] Best Time to Buy and Sell Stock
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+ *
+ * algorithms
+ * Easy (53.24%)
+ * Likes:    12838
+ * Dislikes: 456
+ * Total Accepted:    1.8M
+ * Total Submissions: 3.3M
+ * Testcase Example:  '[7,1,5,3,6,4]'
+ *
+ * You are given an array prices where prices[i] is the price of a given stock
+ * on the i^th day.
+ * 
+ * You want to maximize your profit by choosing a single day to buy one stock
+ * and choosing a different day in the future to sell that stock.
+ * 
+ * Return the maximum profit you can achieve from this transaction. If you
+ * cannot achieve any profit, return 0.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: prices = [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit
+ * = 6-1 = 5.
+ * Note that buying on day 2 and selling on day 1 is not allowed because you
+ * must buy before you sell.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: prices = [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transactions are done and the max profit =
+ * 0.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= prices.length <= 10^5
+ * 0 <= prices[i] <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        
+        for (int i = 0; i < prices.length; i++) {
+            if (minPrice > prices[i]) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+}
+// @lc code=end
+
---
+++ 350.intersection-of-two-arrays-ii.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/*
+ * @lc app=leetcode id=350 lang=java
+ *
+ * [350] Intersection of Two Arrays II
+ *
+ * https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+ *
+ * algorithms
+ * Easy (54.16%)
+ * Likes:    3623
+ * Dislikes: 618
+ * Total Accepted:    684K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * Given two integer arrays nums1 and nums2, return an array of their
+ * intersection. Each element in the result must appear as many times as it
+ * shows in both arrays and you may return the result in any order.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2,2]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [4,9]
+ * Explanation: [9,4] is also accepted.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums1.length, nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 1000
+ * 
+ * 
+ * 
+ * Follow up:
+ * 
+ * 
+ * What if the given array is already sorted? How would you optimize your
+ * algorithm?
+ * What if nums1's size is small compared to nums2's size? Which algorithm is
+ * better?
+ * What if elements of nums2 are stored on disk, and the memory is limited such
+ * that you cannot load all elements into the memory at once?
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int index1 = 0, index2 = 0, k = 0;
+        
+        while (index1 < nums1.length && index2< nums2.length) {
+            if (nums1[index1] < nums2[index2]) {
+                index1++;
+            } else if (nums1[index1] > nums2[index2]) {
+                index2++;
+            } else {
+                nums1[k++] = nums1[index1++];
+                index2++;
+            }
+        }
+
+        return Arrays.copyOfRange(nums1, 0, k);
+    }
+}
+// @lc code=end
+
