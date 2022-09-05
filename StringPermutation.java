4a5dd430d4f16669264ff3c00884a0c6341ebdd1
Zhou Yuchao <yuchaozh1991@gmail.com>
2022-09-04 22:27:10 -0700

    add

---
+++ StringPermutation.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class StringPermutation {
+    public static boolean findPermutation(String str, String pattern) {
+        int windowStart = 0, matched = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : pattern.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            if (map.containsKey(rightChar)) {
+                map.put(rightChar, map.get(rightChar) - 1);
+                if(map.get(rightChar) == 0) {
+                    matched++;
+                }
+            }
+
+            if (matched == map.size()) {
+                return true;
+            }
+            
+            if (windowEnd >= pattern.length() - 1) {
+                char leftChar = str.charAt(windowStart);
+                if (map.containsKey(leftChar)) {
+                    map.put(leftChar, map.get(leftChar) + 1);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Permutation exist: "
+                + StringPermutation.findPermutation("oidbcaf", "abc"));
+        System.out.println("Permutation exist: "
+                + StringPermutation.findPermutation("odicf", "dc"));
+        System.out.println("Permutation exist: "
+                + StringPermutation.findPermutation("bcdxabcdy", "bcdyabcdx"));
+        System.out.println("Permutation exist: "
+                + StringPermutation.findPermutation("aaacb", "abc"));
+    }
+}
