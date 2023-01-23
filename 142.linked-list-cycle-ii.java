82da3b665b8cc02e561bbef957aa901a7074eee5
Zhou Yuchao <yuchaozhou2014@u.northwestern.edu>
2023-01-22 23:15:38 -0800

    Create 142.linked-list-cycle-ii.java

---
+++ 142.linked-list-cycle-ii.java
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=142 lang=java
+ *
+ * [142] Linked List Cycle II
+ *
+ * https://leetcode.com/problems/linked-list-cycle-ii/description/
+ *
+ * algorithms
+ * Medium (45.47%)
+ * Likes:    9986
+ * Dislikes: 733
+ * Total Accepted:    919.3K
+ * Total Submissions: 2M
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given the head of a linked list, return the node where the cycle begins. If
+ * there is no cycle, return null.
+ * 
+ * There is a cycle in a linked list if there is some node in the list that can
+ * be reached again by continuously following the next pointer. Internally, pos
+ * is used to denote the index of the node that tail's next pointer is
+ * connected to (0-indexed). It is -1 if there is no cycle. Note that pos is
+ * not passed as a parameter.
+ * 
+ * Do not modify the linked list.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: tail connects to node index 1
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2], pos = 0
+ * Output: tail connects to node index 0
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = [1], pos = -1
+ * Output: no cycle
+ * Explanation: There is no cycle in the linked list.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of the nodes in the list is in the range [0, 10^4].
+ * -10^5 <= Node.val <= 10^5
+ * pos is -1 or a valid index in the linked-list.
+ * 
+ * 
+ * 
+ * Follow up: Can you solve it using O(1) (i.e. constant) memory?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        
+    }
+}
+// @lc code=end
+
