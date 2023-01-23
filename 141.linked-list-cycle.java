4bf741122f1d82ff68ae1c02a1163b7b8f475581
Zhou Yuchao <yuchaozhou2014@u.northwestern.edu>
2023-01-22 23:13:10 -0800

    Update 141.linked-list-cycle.java

--- 141.linked-list-cycle.java
+++ 141.linked-list-cycle.java
@@ -64,6 +64,7 @@
  * Can you solve it using O(1) (i.e. constant) memory?
  * 
  */
+
 /**
  * Definition for singly-linked list.
  * class ListNode {
@@ -75,6 +76,19 @@
  *     }
  * }
  */
+
+//Solution 1: two pointers
+// https://leetcode.cn/problems/linked-list-cycle/solution/huan-xing-lian-biao-by-leetcode-solution/
+/**
+复杂度分析
+
+时间复杂度：O(N)，其中 N 是链表中的节点数。
+当链表中不存在环时，快指针将先于慢指针到达链表尾部，链表中每个节点至多被访问两次。
+当链表中存在环时，每一轮移动后，快慢指针的距离将减小一。而初始距离为环的长度，因此至多移动 N 轮。
+
+空间复杂度：O(1)。我们只使用了两个指针的额外空间。
+*/
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
         if (head == null || head.next == null) {
@@ -93,3 +107,28 @@ public class Solution {
     }
 }
 
+// Solution 2: HasSet
+/**
+复杂度分析
+
+时间复杂度：O(N)，其中 N 是链表中的节点数。最坏情况下我们需要遍历每个节点一次。
+
+空间复杂度：O(N)，其中 N 是链表中的节点数。主要为哈希表的开销，最坏情况下我们需要将每个节点插入到哈希表中一次。
+*/
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        Set<ListNode> seen = new HashSet<>();
+        while (head != null) {
+            if (!seen.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+
+        return false;
+    }
+}
