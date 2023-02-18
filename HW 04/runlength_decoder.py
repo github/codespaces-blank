6cba7d41bc1bd0f94f6a9db8066416ddc0d2cc66
cyber-shah <120687985+cyber-shah@users.noreply.github.com>
2023-02-17 20:22:27 -0500

    Add files via upload

---
+++ HW 04/runlength_decoder.py
@@ -0,0 +1,56 @@
+import hw4data as hw4
+
+
+def decode(list):
+
+    """
+    # initiate a counter
+    d, i = 0
+
+    # while d is less than all the data lists,
+    # wanted to use a function that access the number of data lists variable
+    # that updates with additions of more data lists
+    while d < 5:
+        # a new list called as decoded_data[i]
+        decoded_data[d] = []
+        while i < len(hw4.DATA[d]):
+            j = 0
+            while j < hw4.DATA0[i + 1]:
+                decoded_data[d].append(hw4.DATA[d][i])
+                j = j + 1
+            i = i + 2
+        print(decoded_data[0])
+    """
+    # initiate empty list
+    decoded_data = []
+
+    # while i is less than list length,
+    i = 0
+    while i < len(list):
+        # append copies of the item in list[i]
+        # list [i+1] times
+        j = 0
+        while j < list[i+1]:
+            decoded_data.append(list[i])
+            j = j + 1
+        # jump to next even number item in list
+        i = i + 2
+    print (decoded_data)
+    return decoded_data
+
+
+def main():
+
+    # can i use a while loop here to iterate all data sets?
+    decoded_data0 = decode(hw4.DATA0)
+    decoded_data1 = decode(hw4.DATA1)
+    decoded_data3 = decode(hw4.DATA3)
+    decoded_data4 = decode(hw4.DATA4)
+    decoded_data5 = decode(hw4.DATA5)
+
+    all_decoded_data = decoded_data0 + decoded_data1 + decoded_data3 + decoded_data4 + decoded_data5
+    single_string = "".join(all_decoded_data)
+
+    print(single_string)
+
+main()
