c1b16e1121361104bae23e3af29582a20900d306
cyber-shah <120687985+cyber-shah@users.noreply.github.com>
2023-02-19 02:36:05 +0000

    cards

--- HW 04/cards.py
+++ HW 04/cards.py
@@ -34,18 +34,15 @@ def shuffle(cards: list) -> list:
     Returns:
         list: _description_
     """
-    
     shuffled_cards = []
     random_list = []
-    
     #initiate a counter
     i,j = 0,0
-    
-    # firstly, we will create a list of random numbers 
+    # firstly, we will create a list of random numbers
     # from 0 to the length of the cards list = 52
     while i < len(cards):
         num = random.randrange(len(cards))
-        # to avoid duplicates, we will append the number to the list only if 
+        # to avoid duplicates, we will append the number to the list only if
         # it is not already in the list
         if num not in random_list:
             random_list.append(num)
@@ -59,12 +56,74 @@ def shuffle(cards: list) -> list:
         j = j + 1
     return shuffled_cards
 
+def deal(number_of_hands : int, number_of_cards : int, cards : list) -> list:
+    i,j,k = 0,0,0
+    dealt_cards = []
+    
+    # create a list to store remainig cards
+    remaining_cards = cards.copy()
+    
+    # check if the number of hands is less than or equal to 4
+    if number_of_hands <= 4:
+        
+        # check if the number of cards is less than or equal to 13
+        if number_of_cards <= 13:
+            
+            while i < number_of_hands:
+                new_list = []
+                
+                # everytime i loop runs, we increase the value of j by 1
+                # first loop would be   1,5,9
+                # second loop would be  2,6,10
+                # third loop would be   3,7,11
+                j = 0
+                
+                # first card to be added into a hand would
+                # always be the card at index i
+                k = i
+                
+                while j < number_of_cards:
+                    # append the card at index k to the new list
+                    new_list.append(cards[k])
+                    
+                    # remove the card at index k from the remaining cards list
+                    # without modifying the original list
+                    if i == (number_of_hands - 1):
+                        del remaining_cards[0]
+                    else:
+                        if j == 0 :
+                            a = 0
+                            del remaining_cards[a]
+                        elif j == 1:
+                            a = number_of_hands - i - 1
+                            del remaining_cards[a]
+                        elif j > 1:
+                            index = j*a
+                            del remaining_cards[index]
+                    # jump to the card at index k + number_of_hands
+                    k = k + number_of_hands
+                    j = j + 1
+                
+                # append one hand to the list before moving to the next hand
+                dealt_cards.append(new_list)
+                i = i + 1
+            return (dealt_cards, remaining_cards)
+        else:
+            print("Please enter a number of cards less than or equal to 13")
+    else:
+        print("Please enter a number of hands less than or equal to 4")
+        
+
 def main():
     """_summary_ This is the main function that calls the create_deck function
     """
     deck = create_deck()
-    print(deck)
+    print(f"Original Deck: \n{deck}")
+    
     shuffled_deck = shuffle(deck)
-    print(f" Shuffled Cards = {shuffled_deck}")
-
+    print(f" Shuffled Deck: \n{shuffled_deck}")
+    
+    dealt_hands, remaining_cards = deal(4,12,shuffled_deck)
+    print(f"Dealt Hands: \n{dealt_hands}")
+    print(f"Remaining cards: \n{remaining_cards}")
 main()
