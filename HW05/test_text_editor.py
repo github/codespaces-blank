af0b1c9d44a5694e87fb31d3c699279dd7ddcae3
cyber-shah <120687985+cyber-shah@users.noreply.github.com>
2023-03-08 15:41:59 +0000

    with a helper function = split

--- HW05/test_text_editor.py
+++ HW05/test_text_editor.py
@@ -1,6 +1,4 @@
 import text_editor as t_editor
-    """_summary_
-    """
 
 def test_append_text(case_number: int, t_original: str,
                      t_append: str, t_expected: str)-> str:
@@ -18,10 +16,6 @@ def test_append_text(case_number: int, t_original: str,
     print (f"\nAppend text, Test Case {case_number}:\
            \nOut:{t_actual}\nExp:{t_expected}")
 
-
-
-
-
 def test_insert_text(case_number: int, t_original: str,
                      t_insert: str, position: int, t_expected: str)-> str:
     """_summary_
@@ -38,31 +32,67 @@ def test_insert_text(case_number: int, t_original: str,
     print (f"\nInsert Text, Test Case {case_number}:\
            \nOut:{t_actual}\nExp:{t_expected}")
 
+def test_substitute_text(case_number: int, t_original: str, 
+                         t_sub: str, t_new: str, t_expected: str)-> str:
+    """_summary_
 
+    Args:
+        case_number (int): _description_
+        t_original (str): _description_
+        t_sub (str): _description_
+        t_new (str): _description_
+        t_expected (str): _description_
+
+    Returns:
+        str: _description_
+    """
+    t_actual = t_editor.substitute_text(t_original, t_sub, t_new)
+    print (f"\nSubstitute Text, Test Case {case_number}:\
+           \nOut:{t_actual}\nExp:{t_expected}")
 
 def main():
-    """_summary_
     """
-    # Test Cases for append_text:
-    test_append_text(0,"Welcome", "to CS-5001", "Welcome to CS-5001") # Ideal case
-    test_append_text(1,"Hello World", "", "Hello World") # Empty append
-    test_append_text(2,"", "to CS-5001", " to CS-5001") # Empty original
+    _summary_
+    """
+    print("------------------ Test Cases for append_text ------------------")
+    # Ideal case
+    test_append_text(0,"Welcome", "to CS-5001", "Welcome to CS-5001")
+    # Empty append
+    test_append_text(1,"Hello World", "", "Hello World")
+    # Empty original
+    test_append_text(2,"", "to CS-5001", " to CS-5001")
 
 
-    # Test Cases for insert_text
+    print("\n\n------------------ Test Cases for insert_text ------------------")
     t_original = "I like apples"
     t_insert = "###"
-    
     # Ideal case
     test_insert_text(0, t_original, t_insert, 1, "I ### like apple")
     # Insert at beginning
     test_insert_text(1, t_original, t_insert, 0, " ### I like apples")
-    # Greater than 3
-    test_insert_text(2, t_original, t_insert, 5, "I like apples ###")
     # Insert at end
-    test_insert_text(3, t_original, t_insert, 3, "I like apples ###")
+    test_insert_text(2, t_original, t_insert, 3, "I like apples ###")
+    # Greater than 3
+    test_insert_text(3, t_original, t_insert, 5, "I like apples ###")
     # Negative position
     test_insert_text(4, t_original, t_insert, -1, "### I like apples")
+    
+    
+    print("\n\n--------------- Test Cases for substitute_text ---------------")
+    t_original = "I like apples."
+    t_new = "oranges"
+    # Ideal case
+    test_substitute_text(0, t_original, "apples", t_new, "I like oranges")
+    # Sub not in original
+    test_substitute_text(1, t_original, "bananas", t_new, "I like apples")
+    # Empty original
+    test_substitute_text(2, "", "apples", t_new, "")
+    # Empty new
+    test_substitute_text(3, t_original, "apples" , " ", "I like ")
+    # Case senstive
+    test_substitute_text(4, t_original, "Apples", t_new, "I like apples.")
+    
+    
 
 if __name__ == "__main__":
     main()
--- HW05/text_editor.py
+++ HW05/text_editor.py
@@ -1,3 +1,39 @@
+def split_text(t_original: str) -> list:
+    """
+    This function splits the text into a list of words, based on
+    " ", ".", "\n", "\t" as delimiters. I had to create this becasue
+    the split() function in python does not split multiple delimiters
+    together.
+    
+    Args:
+        t_original (str): Original text to be split
+
+    Returns:
+        split_list (list): List has split words based on delimiters
+    """
+    i, j = 0, 0
+    split_list = []
+    
+    # this loop runs = len of string
+    while i < len(t_original):
+        # if the character is a delimiter
+        if t_original[i] in [" ", ".", "\n", "\t"]:
+            # create a word from j to i
+            split_word = t_original[j:i]
+            # append only if it is not empty string
+            if split_word != "":
+                split_list.append(split_word)
+            
+            if t_original[i] == ".":
+                split_list.append(".")
+            j = i + 1
+        i += 1
+        
+    if j < len(t_original):  
+        split_list.append(t_original[j:])
+        
+    return split_list       
+
 def append_text(t_original: str, t_append: str)-> str:
     """_summary_
 
@@ -27,4 +63,37 @@ def insert_text(t_original: str, t_insert: str, position: int)-> str:
     if position < 0:
         return t_insert + " " + t_original
     return split_1 + " " + t_insert + " " + split_2
+
+
+def substitute_text(t_original: str, t_sub: str, t_new: str)-> str:
+    """_summary_
+
+    Args:
+        t_original (str): _description_
+        t_sub (str): _description_
+        t_new (str): _description_
+
+    Returns:
+        str: _description_
+    """
+    i = 0
+    t_split = t_original.split()
+    # learn't that for loop iterates
+    # over a copy of the list
+    # hence, we use while loop
+    while i < len(t_split):
+        if t_split[i] == t_sub:
+            t_split[i] = t_new
+        i += 1
+
+    return " ".join(t_split)
+
+
+
+def main():
+    a = split_text("Hellow NEU. Welcome to CS-5001.")
+    print (a)
     
+
+if __name__ == "__main__":
+    main()
