5f7d81513f27103b5123a3ac1789d1f258a2ddbd
CODE SEARCH <150044321+CodeSearchDev@users.noreply.github.com>
2024-11-17 22:30:55 +0530

    Update tools

---
+++ TanuMusic/plugins/tools/draw.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+from HorridAPI import Mango
+
+@app.on_message(filters.command("draw"))
+def draw(client, message):
+    prompt = " ".join(message.command[1:]) if len(message.command) > 1 else None
+    if not prompt:
+        return message.reply_text("ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ǫᴜᴇʀʏ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ᴀɴ ɪᴍᴀɢᴇ ᴜsɪɴɢ /draw ᴄᴏᴍᴍᴀɴᴅs.")
+    try:
+        response = Mango.images.generate(model="flux-1.1-pro", prompt=prompt)
+        message.reply_photo(photo=response.url, caption=" ɪᴍᴀɢᴇ ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ˹ᴛᴀɴᴜ ꭙ ᴍᴜsɪᴄ™♡゙")
+    except Exception as e:
+        message.reply_text(f"ɢᴇᴛᴛɪɴɢ ᴇʀʀᴏʀ: {e}")
