7186b2a0b17c7ec5e3916a4966b614bf6d892232
CODE SEARCH <150044321+CodeSearchDev@users.noreply.github.com>
2024-11-17 22:45:09 +0530

    Update git.py

--- TanuMusic/core/git.py
+++ TanuMusic/core/git.py
@@ -68,4 +68,4 @@ def git():
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
         install_req("pip3 install --no-cache-dir -r requirements.txt")
-        LOGGER(__name__).info(f"Fetching updates from upstream repository...")
+        LOGGER(__name__).info(f"❖Fetching updates from upstream repository...❤️")
