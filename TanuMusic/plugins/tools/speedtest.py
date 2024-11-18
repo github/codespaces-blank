084636cab2e5420ac13bd102664ee653fcbd6060
CODE SEARCH <150044321+CodeSearchDev@users.noreply.github.com>
2024-11-18 17:10:53 +0530

    Update speedtest.py

--- TanuMusic/plugins/tools/speedtest.py
+++ TanuMusic/plugins/tools/speedtest.py
@@ -25,7 +25,7 @@ def testspeed(m, _):
     return result
 
 
-@app.on_message(filters.command(["speedtest", "spt", "ping"]))
+@app.on_message(filters.command(["speedtest", "spt"]))
 @language
 async def speedtest_function(client, message: Message, _):
     m = await message.reply_text(_["server_11"])
