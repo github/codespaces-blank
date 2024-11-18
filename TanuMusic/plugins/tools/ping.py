a5f056df5892c75184713137d0db85d83e0f6c23
CODE SEARCH <150044321+CodeSearchDev@users.noreply.github.com>
2024-11-18 17:14:37 +0530

    Update ping.py

--- TanuMusic/plugins/tools/ping.py
+++ TanuMusic/plugins/tools/ping.py
@@ -19,7 +19,7 @@ async def ping_com(client, message: Message, _):
         photo=PING_IMG_URL,
         caption=_["ping_1"].format(app.mention),
     )
-    pytgping = await Anony.ping()
+    pytgping = await Tanu.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
