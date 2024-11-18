4aad8d718b1fdff256400e4c747762de95b1402f
CODE SEARCH <150044321+CodeSearchDev@users.noreply.github.com>
2024-11-18 17:10:39 +0530

    Update tools

---
+++ TanuMusic/plugins/tools/ping.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from TanuMusic import app
+from TanuMusic.core.call import Anony
+from TanuMusic.utils import bot_sys_stats
+from TanuMusic.utils.decorators.language import language
+from TanuMusic.utils.inline import supp_markup
+from config import BANNED_USERS, PING_IMG_URL
+
+
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"].format(app.mention),
+    )
+    pytgping = await Anony.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
+    )
