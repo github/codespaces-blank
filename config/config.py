4db441878c907788a2902ac521d313bc1a58c5e6
CODE SEARCH <150044321+CodeSearchDev@users.noreply.github.com>
2024-11-26 19:10:09 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -55,7 +55,7 @@ GIT_TOKEN = getenv(
 
 #❖ Make your bots privacy from telegra.ph and put your url here
 PRIVACY_LINK = getenv(
-    "PRIVACY_LINK", "https://telegra.ph/"
+    "PRIVACY_LINK", "https://graph.org/Privacy-Policy-for-Tanu-Music-11-26"
 )
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/C0DE_SEARCH")
