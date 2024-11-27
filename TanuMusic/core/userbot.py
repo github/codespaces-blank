b4a73f7b3dff454e1563a9f36a63e87938c247c7
CODE SEARCH <150044321+CodeSearchDev@users.noreply.github.com>
2024-11-27 19:26:51 +0530

    Update userbot.py

--- TanuMusic/core/userbot.py
+++ TanuMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("krishnetwork")
-                await self.one.join_chat("krishsupport")
+                await self.one.join_chat("C0DE_SEARCH")
+                await self.one.join_chat("AsuraaSupports")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("krishnetwork")
-                await self.one.join_chat("krishsupport")
+                await self.two.join_chat("C0DE_SEARCH")
+                await self.one.join_chat("AsuraaSupports")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("krishnetwork")
-                await self.one.join_chat("krishsupport")
+                await self.three.join_chat("C0DE_SEARCH")
+                await self.one.join_chat("AsuraaSupports")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("krishnetwork")
-                await self.one.join_chat("krishsupport")
+                await self.four.join_chat("C0DE_SEARCH")
+                await self.one.join_chat("AsuraaSupports")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("krishnetwork")
-                await self.one.join_chat("krishsupport")
+                await self.five.join_chat("C0DE_SEARCH")
+                await self.one.join_chat("AsuraaSupports")
             except:
                 pass
             assistants.append(5)
