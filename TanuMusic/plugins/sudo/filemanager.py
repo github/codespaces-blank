c14b26afa43fa03fbe57ebd1207846ccb8994a54
CODE SEARCH <150044321+CodeSearchDev@users.noreply.github.com>
2024-11-29 17:19:28 +0530

    Update filemanager.py

--- TanuMusic/plugins/sudo/filemanager.py
+++ TanuMusic/plugins/sudo/filemanager.py
@@ -1,7 +1,7 @@
 import io
 import os
 import time
-from inspect import getfullargspec
+from inspect import signature
 from os.path import exists, isdir
 
 from pyrogram import filters
@@ -33,15 +33,14 @@ async def eor(msg: Message, **kwargs):
     else:
         func = msg.reply
 
-    # Ensure we only pass valid arguments
-    spec = getfullargspec(func).args
-    valid_kwargs = {k: v for k, v in kwargs.items() if k in spec}
+    # Get valid arguments for the function
+    valid_args = signature(func).parameters.keys()
+    filtered_kwargs = {k: v for k, v in kwargs.items() if k in valid_args}
 
-    # Call the function with await
-    return await func(**valid_kwargs)
+    return await func(**filtered_kwargs)
 
 
-async def capture_err(func):
+def capture_err(func):
     """Decorator to handle errors gracefully."""
     async def wrapper(client, message, *args, **kwargs):
         try:
@@ -49,8 +48,7 @@ async def capture_err(func):
         except Exception as e:
             error_feedback = split_limits(str(e))
             for feedback in error_feedback:
-                if isinstance(message, Message):
-                    await message.reply(feedback)
+                await message.reply(feedback)
             raise e
     return wrapper
 
@@ -58,8 +56,6 @@ async def capture_err(func):
 @app.on_message(filters.command("ls") & ~filters.forwarded & ~filters.via_bot & SUDOERS)
 @capture_err
 async def lst(_, message):
-    prefix = message.text.split()[0][0]
-    chat_id = message.chat.id
     path = os.getcwd()
     text = message.text.split(" ", 1)
     directory = None
@@ -76,18 +72,15 @@ async def lst(_, message):
         return
 
     if isdir(path):
-        if directory:
-            msg = f"Folders and Files in `{path}` :\n\n"
-            lists = os.listdir(path)
-        else:
-            msg = "Folders and Files in Current Directory:\n\n"
-            lists = os.listdir(path)
-
+        msg = f"Folders and Files in `{path}`:\n\n" if directory else "Folders and Files in Current Directory:\n\n"
         files = ""
         folders = ""
-        for contents in sorted(lists):
+
+        for contents in sorted(os.listdir(path)):
             item_path = os.path.join(path, contents)
-            if not isdir(item_path):
+            if isdir(item_path):
+                folders += f"📁 {contents}\n"
+            else:
                 size = os.stat(item_path).st_size
                 if contents.endswith((".mp3", ".flac", ".wav", ".m4a")):
                     files += f"🎵 {contents}\n"
@@ -109,49 +102,46 @@ async def lst(_, message):
                     files += f"🐍 {contents}\n"
                 else:
                     files += f"📄 {contents}\n"
-            else:
-                folders += f"📁 {contents}\n"
 
-        if files or folders:
-            msg += folders + files
-        else:
-            msg += "__empty path__"
+        msg += folders + files if files or folders else "__empty path__"
     else:
         size = os.stat(path).st_size
-        msg = "The details of the given file:\n\n"
-        if path.endswith((".mp3", ".flac", ".wav", ".m4a")):
-            mode = "🎵 "
-        elif path.endswith(".opus"):
-            mode = "🎙 "
-        elif path.endswith((".mkv", ".mp4", ".webm", ".avi", ".mov", ".flv")):
-            mode = "🎞 "
-        elif path.endswith((".zip", ".tar", ".tar.gz", ".rar", ".7z", ".xz")):
-            mode = "🗜 "
-        elif path.endswith((".jpg", ".jpeg", ".png", ".gif", ".bmp", ".ico", ".webp")):
-            mode = "🖼 "
-        elif path.endswith((".exe", ".deb")):
-            mode = "⚙️ "
-        elif path.endswith((".iso", ".img")):
-            mode = "💿 "
-        elif path.endswith((".apk", ".xapk")):
-            mode = "📱 "
-        elif path.endswith(".py"):
-            mode = "🐍 "
-        else:
-            mode = "📄 "
-
         last_modified = time.ctime(os.path.getmtime(path))
         last_accessed = time.ctime(os.path.getatime(path))
-        msg += f"<b>Location:</b> {path}\n"
-        msg += f"<b>Icon:</b> {mode}\n"
-        msg += f"<b>Size:</b> {humanbytes(size)}\n"
-        msg += f"<b>Last Modified Time:</b> {last_modified}\n"
-        msg += f"<b>Last Accessed Time:</b> {last_accessed}"
+
+        if path.endswith((".mp3", ".flac", ".wav", ".m4a")):
+            mode = "🎵"
+        elif path.endswith(".opus"):
+            mode = "🎙"
+        elif path.endswith((".mkv", ".mp4", ".webm", ".avi", ".mov", ".flv")):
+            mode = "🎞"
+        elif path.endswith((".zip", ".tar", ".tar.gz", ".rar", ".7z", ".xz")):
+            mode = "🗜"
+        elif path.endswith((".jpg", ".jpeg", ".png", ".gif", ".bmp", ".ico", ".webp")):
+            mode = "🖼"
+        elif path.endswith((".exe", ".deb")):
+            mode = "⚙️"
+        elif path.endswith((".iso", ".img")):
+            mode = "💿"
+        elif path.endswith((".apk", ".xapk")):
+            mode = "📱"
+        elif path.endswith(".py"):
+            mode = "🐍"
+        else:
+            mode = "📄"
+
+        msg = (
+            f"<b>Location:</b> {path}\n"
+            f"<b>Icon:</b> {mode}\n"
+            f"<b>Size:</b> {humanbytes(size)}\n"
+            f"<b>Last Modified:</b> {last_modified}\n"
+            f"<b>Last Accessed:</b> {last_accessed}\n"
+        )
 
     if len(msg) > MAX_MESSAGE_SIZE_LIMIT:
         with io.BytesIO(str.encode(msg)) as out_file:
             out_file.name = "ls.txt"
-            await app.send_document(chat_id, out_file, caption=path)
+            await app.send_document(message.chat.id, out_file, caption=path)
             await message.delete()
     else:
         await eor(message, text=msg)
@@ -159,12 +149,13 @@ async def lst(_, message):
 
 @app.on_message(filters.command("rm") & ~filters.forwarded & ~filters.via_bot & SUDOERS)
 @capture_err
-async def rm_file(client, message):
+async def rm_file(_, message):
     if len(message.command) < 2:
         return await eor(message, text="Please provide a file name to delete.")
     file = message.text.split(" ", 1)[1]
+
     if exists(file):
         os.remove(file)
-        await eor(message, text=f"{file} has been deleted.")
+        await eor(message, text=f"`{file}` has been deleted.")
     else:
-        await eor(message, text=f"{file} doesn't exist!")
+        await eor(message, text=f"`{file}` doesn't exist!")
