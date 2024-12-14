4e8a6d66c9ac47ddad7d8b1168dee79e48df0507
CODE SEARCH <150044321+CodeSearchDev@users.noreply.github.com>
2024-12-14 22:27:18 +0530

    Update autoleave.py

--- TanuMusic/plugins/misc/autoleave.py
+++ TanuMusic/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1001686672798
-                                and i.chat.id != -1001549206010
+                                and i.chat.id != -1002342994330
+                                and i.chat.id != -1002296968230
                             ):
                                 if left == 20:
                                     continue
