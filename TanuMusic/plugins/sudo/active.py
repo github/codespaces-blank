fd391419744d0d8527873ba12e9663ee02ac5b2a
CODE SEARCH <150044321+CodeSearchDev@users.noreply.github.com>
2024-12-15 18:23:38 +0530

    Update active.py

--- TanuMusic/plugins/sudo/active.py
+++ TanuMusic/plugins/sudo/active.py
@@ -113,7 +113,7 @@ async def start(client: Client, message: Message):
     ac_audio = str(len(await get_active_chats()))
     ac_video = str(len(await get_active_video_chats()))
     await message.reply_text(
-        f"❖ <b>ᴀᴄᴛɪᴠᴇ ᴄʜᴀᴛs ɪɴғᴏ</b> :\n\nᴠᴏɪᴄᴇ : {ac_audio}\nᴠɪᴅᴇᴏ  : {ac_video}",
+        f"❖ <b>ᴀᴄᴛɪᴠᴇ ᴄʜᴀᴛs ɪɴғᴏ</b> :\n\nᴠᴏɪᴄᴇ : {ac_audio}\nᴠɪᴅᴇᴏ : {ac_video}",
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data="close")]]
         ),
